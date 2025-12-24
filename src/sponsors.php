d0144776d17aaa6ead6dca95e476e763c6003d2a
oelaimar <oussama.el.aimar@gmail.com>
2025-12-24 22:52:54 +0100

    feat: add crud to sponsors class

---
+++ src/sponsors.php
@@ -0,0 +1,55 @@
+<?php
+require_once "database.php";
+
+class Sponsors extends Database
+{
+    private ?int $id;
+    private string $name;
+
+    //return an arr you shoud affict it to a variable
+    public function getAllData(): array
+    {
+        $sql = "SELECT * FROM sponsor;";
+        $stmt = $this->connect()->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    public function getById(int $id): void
+    {
+        $sql = "SELECT * FROM sponsor WHERE id = ?";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$id]);
+        $sponsor = $stmt->fetch();
+
+        $this->id = (int)$sponsor['id'];
+        $this->name = (int)$sponsor['name'];
+    }
+
+    public function getName(int $id): string
+    {
+        $this->getById($id);
+        return $this->name;
+    }
+
+    public function save(string $name): bool
+    {
+        $sql = "INSERT INTO player (name) VALUES(?);";
+        $stmt = $this->connect()->prepare($sql);
+
+        return $stmt->execute([$name]);
+    }
+
+    public function delete(int $id): bool
+    {
+        $sql = "DELETE FROM matches WHERE id = ?;";
+        $stmt = $this->connect()->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+
+    public function update(string $name, int $id): bool
+    {
+        $sql = "UPDATE player SET name = ? WHERE id = ?;";
+        $stmt = $this->connect()->prepare($sql);
+        return $stmt->execute([$name, $id]);
+    }
+}
